c6f7e2363eb9388228a9df8fc93c9bce33562948
conkWest <lancearquillano@gmail.com>
2025-11-25 21:26:11 +0800

    GUI of the game itself, VERY barebones WIP

---
+++ src/gui/GUI.java
@@ -0,0 +1,38 @@
+package gui;
+
+import javax.swing.*;
+
+public class GUI {
+    private JPanel mainPanel;
+    private JPanel topPanel;
+    private JPanel centerPanel;
+    private JPanel bottomPanel;
+    private JButton button2;
+    private JPanel roomPanel;
+    private JPanel triplePanel;
+    private JPanel messagePanel;
+    private JButton item1;
+    private JButton item2;
+    private JButton item3;
+    private JButton item4;
+    private JButton item5;
+    private JButton item6;
+    private JButton item7;
+    private JButton item8;
+    private JButton item9;
+    private JList list1;
+    private JLabel selectedItem;
+    private JLabel selectedPuzzle;
+    private JButton pull;
+    private JLabel machineDisplay;
+    private JLabel result;
+    private JButton tryitem;
+    private JButton solvepuzzle;
+    private JButton gethint;
+    private JButton nextroom;
+    private JButton savegame;
+    private JTextArea message;
+    private JLabel roomTitle;
+    private JTextArea roomDesc;
+    private JLabel roomImgPlaceholder;
+}
